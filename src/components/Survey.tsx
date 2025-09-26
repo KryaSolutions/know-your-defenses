df06b59824e1b52599461b4fd7c25ae07bbd7657
TheRootDaemon <harshath3018@gmail.com>
2025-09-26 13:00:16 +0530

    feat: Header, UI improvements

--- src/components/Survey.tsx
+++ src/components/Survey.tsx
@@ -99,7 +99,7 @@ const Survey = () => {
                                 {/* Button */}
                                 <button
                                     onClick={() => handleStartAssessment(assessment)}
-                                    className="px-5 py-2.5 rounded-lg bg-blue-600 text-white font-medium shadow-sm hover:bg-blue-700 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 transition whitespace-nowrap"
+                                    className="px-5 py-2.5 rounded-lg bg-blue-600 text-white font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 transition whitespace-nowrap"
                                     tabIndex={-1}
                                 >
                                     Start Assessment
